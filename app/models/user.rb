6c016de3e39adc48bb6673718b51dd6b9599a29f
ibuki-takeuchi01 <tomica.0324@gmail.com>
2021-07-15 17:42:13 +0900

    sns認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
 
           with_options presence: true do
             validates :nickname
@@ -16,7 +17,22 @@ class User < ApplicationRecord
             validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
       
               has_many :reviews, dependent: :destroy
+              has_many :sns_credentials
 
               extend ActiveHash::Associations::ActiveRecordExtensions
               belongs_to :prefecture
+
+              def self.from_omniauth(auth)
+                sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+                user = User.where(email: auth.info.email).first_or_initialize(
+                  nickname: auth.info.name,
+                    email: auth.info.email
+                )
+
+                if user.persisted?
+                  sns.user = user
+                  sns.save
+                end
+                { user: user, sns: sns }
+              end
       end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     registrations: 'owners/registrations'
   }
   devise_for :users, controllers: {
-    sessions:      'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
 
